037e5c4583dbd2a9bc113842e1310f9bc660b6e8
Anna Shia <chiu338920@gmail.com>
2021-03-29 21:05:31 +0800

    first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chanchu-ting/PycharmProjects/inventory-management/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
